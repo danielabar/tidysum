2d17f53eeda8892bdeb87709737e63298a6cd6e2
Daniela Baron <baron.daniela@gmail.com>
2018-10-21 19:03:25 -0400

    organize output by year

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const argv = require('yargs').argv;
+const logger = require('./lib/logger');
 const processExpenses = require('./lib/process-expenses');
 
 // TODO Figure out yargs required args, version and help usage
@@ -7,6 +8,8 @@ if (!argv.expenseFile) {
   console.log('Specify path to expenseFile'); // eslint-disable-line no-console
 } else {
   (async () => {
-    await processExpenses.processFile(argv.expenseFile);
+    const result = await processExpenses.processFile(argv.expenseFile);
+    // TODO write result to output file
+    logger.info('result', { data: result });
   })();
 }
