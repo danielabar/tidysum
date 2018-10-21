b5876ef947029700869ebeea514b54d737006395
Daniela Baron <baron.daniela@gmail.com>
2018-10-21 18:43:13 -0400

    add logging

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const processExpenses = require('./lib/process-expenses');
 
 // TODO Figure out yargs required args, version and help usage
 if (!argv.expenseFile) {
-  console.log('Specify path to expenseFile');
+  console.log('Specify path to expenseFile'); // eslint-disable-line no-console
 } else {
-  // await processExpenses.processFile(argv.expenseFile);
   (async () => {
     await processExpenses.processFile(argv.expenseFile);
   })();
