cb2e844e385ee2161e32879dcc112fd4cb3217da
Daniela Baron <baron.daniela@gmail.com>
2018-11-03 13:23:40 -0400

    better use of yargs and make it a cli

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
-const argv = require('yargs').argv;
+const argv = require('yargs')
+  .usage('Usage: $0 option expenseFile \n e.g $0 -e /path/to/csv')
+  .alias('e', 'expenseFile')
+  .nargs('e', 1)
+  .describe('e', 'Path to csv file containing expenses')
+  .demandOption(['e'])
+  .help('h')
+  .alias('h', 'help').argv;
 const logger = require('./lib/logger');
 const processExpenses = require('./lib/process-expenses');
 
-// TODO Figure out yargs required args, version and help usage
-if (!argv.expenseFile) {
-  console.log('Specify path to expenseFile'); // eslint-disable-line no-console
-} else {
-  (async () => {
-    const result = await processExpenses.processFile(argv.expenseFile);
-    // TODO write result to output file
-    logger.info('result', { data: result });
-  })();
-}
+(async () => {
+  const result = await processExpenses.processFile(argv.expenseFile);
+  // TODO write result to output file
+  logger.info('result', { data: result });
+})();
