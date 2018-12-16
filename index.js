78c1bb4db426099b7469fc90ddb27d67cbeaa3d6
Daniela Baron <baron.daniela@gmail.com>
2018-12-16 18:10:30 -0500

    add optional args

--- index.js
+++ index.js
@@ -3,16 +3,27 @@
 // ref: https://scotch.io/@Youngestdev/how-to-build-a-nodejs-commandline-apps-with-yargs
 const fs = require('fs');
 const argv = require('yargs')
-  .usage('Usage: $0 option expenseFile \n e.g $0 -e /path/to/csv')
+  .usage(
+    'Usage:\n    $0 -e /path/to/expense.csv\nWith optional args:\n    $0 -e /path/to/expense.csv -i 2000 -f 900'
+  )
   .alias('e', 'expenseFile')
   .nargs('e', 1)
   .describe('e', 'Path to csv file containing expenses')
   .demandOption(['e'])
+
+  .alias('i', 'monthlyIncome')
+  .nargs('i', 1)
+  .describe('i', 'Monthly income')
+
+  .alias('f', 'fixedExpenses')
+  .nargs('f', 1)
+  .describe('f', 'Monthly fixed expenses')
+
   .help('h')
   .alias('h', 'help').argv;
 const expense = require('./lib/expense');
 
 (async () => {
-  const result = await expense.process(argv.e);
+  const result = await expense.process(argv.e, argv.i, argv.f);
   fs.writeFileSync('expenses.json', JSON.stringify(result, null, 2), 'utf8');
 })();
